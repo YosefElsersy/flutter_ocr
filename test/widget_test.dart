fcae0ca065c60bc94218270d12967a547c19f804
smity <you@example.com>
2025-11-30 19:29:59 +0200

    Updated Test URI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ds/main.dart';
+import 'package:flutter_ocr/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
