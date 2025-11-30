d621e5ade122649ba4b883479697a38217f07cca
smity <you@example.com>
2025-11-30 19:20:22 +0200

    Updated Name

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DS App',
+      title: 'Flutter_OCR',
       home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
